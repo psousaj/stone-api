a75a51c145ddf75418c3737b6e9e73dbebfa343d
José Pinheiro <josepsousa2012@gmail.com>
2023-02-27 17:40:14 -0300

    testes de tela visual

--- teste.py
+++ teste.py
@@ -1,12 +1,32 @@
 import  subprocess
 import os
-import tkinter
+import tkinter as tk
+# from tkinter import *
+from tkinter.ttk import * 
+
+root = tk.Tk()
+root.geometry('250x200')
+root.title("Stone API - Connection")
+
+root.rowconfigure([1], weight=1, minsize=1)
+root.columnconfigure([1], weight=1, minsize=1)
+
+txt1 = tk.Label(text="Digite o código do cliente", height=1).grid(row=0, column=0, sticky="NSEW")
+
+cod = tk.Entry().grid(row=0, column=1, sticky="NSEW")
+
+def bora():
+    print(cod.get())
+
+btn = Button(root, text='Baixar', command = bora).grid(row=1, column=0)
 
 
 
 
 
 
-interpreter = 'python3' if os.name == 'posix' else 'python'
-args = [interpreter, 'main.py', '--code', '0', '--m', 'None', '--a', '']
-subprocess.Popen(args)
+root.mainloop()
+
+# interpreter = 'python3' if os.name == 'posix' else 'python'
+# args = [interpreter, 'main.py', '--code', '0', '--m', 'None', '--a', '']
+# subprocess.Popen(args)
